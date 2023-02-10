1858f3b46440775e042608ca41b4ca56e0bfb2b0
Ljusaj123 <rosana.ljusaj2@gmail.com>
2023-02-10 22:39:41 +0100

    added navigation to icons

--- src/modules/footer/Footer.js
+++ src/modules/footer/Footer.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { BiHomeAlt } from "react-icons/bi";
 import { GoCalendar } from "react-icons/go";
 import { FiClock } from "react-icons/fi";
 
 function Footer() {
+  const navigate = useNavigate();
   return (
     <footer>
-      <BiHomeAlt />
-      <GoCalendar />
-      <FiClock />
+      <BiHomeAlt onClick={() => navigate("/")} />
+      <GoCalendar onClick={() => navigate("/calendar")} />
+      <FiClock onClick={() => navigate("/presence")} />
     </footer>
   );
 }

d784d5ad9475e988fe07c06dc0daff0d9841d960
Ljusaj123 <rosana.ljusaj2@gmail.com>
2023-02-10 16:15:21 +0100

    created routing for pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import Header from "./modules/header/Header";
-import Footer from "./modules/footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Calendar from "./pages/calendar/Calendar";
+import Presence from "./pages/presence/Presence";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/presence" element={<Presence />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./../../modules/header/Header";
+import Footer from "./../../modules/footer/Footer";
+
+function Home() {
+  return (
+    <div className="home">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;

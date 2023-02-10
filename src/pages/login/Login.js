9b0268481cf2000fb093efe8e57b24c474d93afa
Ljusaj123 <rosana.ljusaj2@gmail.com>
2023-02-10 22:31:19 +0100

    created states for username and password, fake login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaFacebookF } from "react-icons/fa";
 import { BiUser, BiShareAlt } from "react-icons/bi";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    if (!username || !password) {
+      alert("you need to enter username and password");
+    } else {
+      alert("You are loged in");
+      setUsername("");
+      setPassword("");
+    }
+  };
   return (
     <div className="login">
       <div className="icon-holder">
@@ -10,19 +22,31 @@ function Login() {
       </div>
 
       <div className="input-holder">
-        <div class="username">
+        <div className="username">
           <BiUser className="icon" />
-          <input type="text" placeholder="FESB korisnički račun" />
+          <input
+            type="text"
+            placeholder="FESB korisnički račun"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
 
-        <div class="password">
+        <div className="password">
           <BiShareAlt className="icon" />
-          <input type="password" placeholder="Lozinka" />
+          <input
+            type="password"
+            placeholder="Lozinka"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
       </div>
 
       <div className="button-holder">
-        <button className="btn btn-color">Prijavi se</button>
+        <button className="btn btn-color" type="submit" onClick={() => login()}>
+          Prijavi se
+        </button>
         <span>Pomoć</span>
       </div>
     </div>

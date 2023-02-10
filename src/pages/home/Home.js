e51f54bd4a454c23bdbd15ca877f9fb3ea51b362
Ljusaj123 <rosana.ljusaj2@gmail.com>
2023-02-10 22:11:47 +0100

    imported lectures module

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./../../modules/header/Header";
 import Footer from "./../../modules/footer/Footer";
+import Lectures from "../../modules/lectures/Lectures";
 
 function Home() {
   return (
     <div className="home">
       <Header />
+      <Lectures />
       <Footer />
     </div>
   );

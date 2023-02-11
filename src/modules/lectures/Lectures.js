267b9800f985b1efcea5735071c562bba7de2915
Ljusaj123 <rosana.ljusaj2@gmail.com>
2023-02-11 23:18:32 +0100

    text-center class, get request for schedule

--- src/modules/lectures/Lectures.js
+++ src/modules/lectures/Lectures.js
@@ -1,31 +1,59 @@
 import React, { useState, useEffect } from "react";
 import Lecture from "../../components/lecture/Lecture";
 import Popup from "../popup/Popup";
-import { schedule } from "../../const/lectures.js";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 
-export default function Lectures({ route }) {
+export default function Lectures() {
   const location = useLocation();
 
-  // get userId
   const { accessToken } = location.state;
 
   const [trigger, setTrigger] = useState(false);
-  const [lectures, setLectures] = useState([schedule]);
+  const [lectures, setLectures] = useState([]);
   const [lecture, setLecture] = useState({});
+  const [filtrirano, setFiltrirano] = useState([]);
+  const [activeEvidentirano, setActiveEvidentirano] = useState(false);
+  const [activeNeevidentirano, setActiveNeevidentirano] = useState(false);
+
+  const handleNeevidentirano = () => {
+    setFiltrirano([]);
+
+    lectures.forEach((lecture) => {
+      if (lecture.presence === false) {
+        setFiltrirano((lec) => [...lec, lecture]);
+      }
+    });
+    setActiveEvidentirano(false);
+    setActiveNeevidentirano(true);
+  };
+
+  const handleEvidentirano = () => {
+    setFiltrirano([]);
+    lectures.forEach((lecture) => {
+      if (lecture.presence === true) {
+        setFiltrirano((evid) => [...evid, lecture]);
+      }
+    });
+    setActiveEvidentirano(true);
+    setActiveNeevidentirano(false);
+  };
 
   useEffect(() => {
-    axios
-      .get("http://localhost:4200/scheduletoday", {
-        headers: {
-          Authorization: accessToken,
-        },
-      })
-      .then((response) => {
-        setLectures(response.data.todaySchedule.kolegiji);
-        console.log(response);
-      });
+    async function FetchSchedule() {
+      await axios
+        .get("http://localhost:4200/scheduletoday", {
+          headers: {
+            Authorization: accessToken,
+          },
+        })
+        .then((response) => {
+          setLectures(response.data.todaySchedule.kolegiji);
+          setFiltrirano(response.data.todaySchedule.kolegiji);
+        });
+    }
+
+    FetchSchedule();
   }, [accessToken]);
 
   const handleClick = (lecture) => {
@@ -35,28 +63,43 @@ export default function Lectures({ route }) {
 
   return (
     <div className="lectures">
-      {console.log(accessToken)}
       <h2 className="text-holder">
-        <button className="btn--no-style">
-          <h2>Evidentirano</h2>
+        <button className="btn--no-style" onClick={() => handleEvidentirano()}>
+          <h2 className={activeEvidentirano ? "active" : ""}>Evidentirano</h2>
         </button>
         <span> | </span>
-        <button className="btn--no-style">
-          <h2>Neevidentirano</h2>
+        <button
+          className="btn--no-style"
+          onClick={() => handleNeevidentirano()}
+        >
+          <h2 className={activeNeevidentirano ? "active" : ""}>
+            Neevidentirano
+          </h2>
         </button>
       </h2>
       <h2>Današnja predavanja</h2>
-      {lectures.map((lecture, index) => {
-        return (
-          <button
-            className="btn--no-style"
-            onClick={() => handleClick(lecture)}
-            key={index}
-          >
-            <Lecture lecture={lecture} />
-          </button>
-        );
-      })}
+
+      {lectures.length === 0 ? (
+        <p className="text-center">Danas nemate obaveza</p>
+      ) : filtrirano.length !== 0 ? (
+        filtrirano.map((lecture, index) => {
+          return (
+            <button
+              className="btn--no-style"
+              onClick={() => handleClick(lecture)}
+              key={index}
+            >
+              <Lecture lecture={lecture} />
+            </button>
+          );
+        })
+      ) : (
+        <p className="text-center">
+          Danas nema {activeEvidentirano ? "evidentiranih" : "neevidentiranih"}{" "}
+          predmeta
+        </p>
+      )}
+
       <Popup trigger={trigger} setTrigger={setTrigger} lecture={lecture} />
     </div>
   );

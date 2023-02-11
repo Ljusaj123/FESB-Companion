f49891354a46fa76dc01aab4846e96c1d42d6bbe
Ljusaj123 <rosana.ljusaj2@gmail.com>
2023-02-11 02:53:39 +0100

    testing data

---
+++ src/const/lectures.js
@@ -0,0 +1,75 @@
+//za svakog studenta oznaceno koje predmete ima taj dan, koje nema
+//0 znaci da nema, 1 da ima
+
+export const schedule = [
+  {
+    id: 1,
+    name: "multimedijski sustavi",
+    time: "17:00-19:00",
+    classroom: "A101",
+    type: "predavanja",
+    presence: false,
+  },
+  {
+    id: 2,
+    name: "multimedijski sustavi",
+    time: "12:00-13:30",
+    classroom: "B420",
+    type: "labovi",
+    presence: true,
+  },
+  {
+    id: 3,
+    name: "grid računalni sustavi",
+    time: "16:00-17:00",
+    classroom: "A102",
+    type: "auditorne",
+    presence: false,
+  },
+  {
+    id: 4,
+    name: "medicinski uređaji",
+    time: "14:00-16:00",
+    classroom: "B301",
+    type: "predavanja",
+    presence: true,
+  },
+];
+
+// export const schedule = [
+//   {
+//     id_studenta: 1,
+//     multimedijski_sustavi: 1,
+//     ugradbeni_racunalni_sustavi: 0,
+//     poslovni_inf_sust: 1,
+//     grid_sustavi: 0,
+//   },
+// {
+//   id_studenta: 2,
+//   multimedijski_sustavi: 1,
+//   ugradbeni_racunalni_sustavi: 0,
+//   poslovni_inf_sust: 1,
+//   grid_sustavi: 1,
+// },
+// {
+//   id_studenta: 3,
+//   multimedijski_sustavi: 1,
+//   ugradbeni_racunalni_sustavi: 0,
+//   poslovni_inf_sust: 1,
+//   grid_sustavi: 0,
+// },
+// {
+//   id_studenta: 4,
+//   multimedijski_sustavi: 1,
+//   ugradbeni_racunalni_sustavi: 0,
+//   poslovni_inf_sust: 1,
+//   grid_sustavi: 1,
+// },
+// {
+//   id_studenta: 5,
+//   multimedijski_sustavi: 1,
+//   ugradbeni_racunalni_sustavi: 0,
+//   poslovni_inf_sust: 1,
+//   grid_sustavi: 1,
+// },
+// ];

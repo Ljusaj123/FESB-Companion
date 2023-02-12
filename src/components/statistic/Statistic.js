7c500500f27c4c93a6f4f156cf865d7310a2cef0
„michellesaric“ <msaric03@fesb.hr>
2023-02-12 22:35:22 +0100

    Added css for the mapping, scrollable component and pie chart. Didn't exactly know how to style it tho

--- src/components/statistic/Statistic.js
+++ src/components/statistic/Statistic.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
+import "chart.js/auto";
+import { Doughnut } from "react-chartjs-2";
+import { data } from "../../const/dougnutData";
 
 function Statistic({ subjectPresence }) {
+  const ref = useRef();
   return (
     <div className="statistic">
       {subjectPresence.map((presence) => {
@@ -10,6 +14,7 @@ function Statistic({ subjectPresence }) {
             <h2 className="subject-presence">
               <span className="presence-number">{presence.present}</span>/13
             </h2>
+            <Doughnut ref={ref} data={data} />
           </div>
         );
       })}

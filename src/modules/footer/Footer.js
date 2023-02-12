be613a88306024e5c2ef103f0bc299d8ca3b361c
Ljusaj123 <rosana.ljusaj2@gmail.com>
2023-02-13 00:01:05 +0100

    active icon set

--- src/modules/footer/Footer.js
+++ src/modules/footer/Footer.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { BiHomeAlt } from "react-icons/bi";
 import { GoCalendar } from "react-icons/go";
 import { FiClock } from "react-icons/fi";
 
 function Footer() {
   const navigate = useNavigate();
+  const pathname = window.location.pathname;
+  const { id } = useParams();
+
+  useEffect(() => {
+    const icons =document.querySelectorAll(".icons-navigate");
+    icons.forEach((day)=> day.classList.remove("active") );
+
+    switch (pathname) {
+      case "/home":
+        document.getElementById("home").classList.add("active");
+        break;
+      case "/calendar":
+        document.getElementById("calendar").classList.add("active");
+        break;
+      case "/presence":
+        document.getElementById("presence").classList.add("active");
+        break;
+      case `/presence/${id}`:
+        document.getElementById("presence").classList.add("active");
+        break;
+      default:
+    }
+  }, [id, pathname]);
+
   return (
     <footer>
-      <BiHomeAlt onClick={() => navigate("/home")} />
-      <GoCalendar onClick={() => navigate("/calendar")} />
-      <FiClock onClick={() => navigate("/presence")} />
+      <BiHomeAlt id="home" class="icons-navigate" onClick={() => navigate("/home")} />
+      <GoCalendar id="calendar" class="icons-navigate" onClick={() => navigate("/calendar")} />
+      <FiClock id="presence" class="icons-navigate" onClick={() => navigate("/presence")} />
     </footer>
   );
 }
